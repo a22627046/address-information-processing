5f811b3d1049afcbc9e1de1dce604ec1c4ce7ff3
Yu-Ting <a22627046@gmail.com>
2025-08-25 02:34:27 +0800

    project init

---
+++ main.py
@@ -0,0 +1,20 @@
+import csv
+
+with open('地址資料.csv') as infile:
+    data = list(csv.DictReader(infile))
+    for e in data:
+        print('原始資料:', e['姓名'], e['縣市'], e['住址'])
+        if e['縣市'][0] == '台':
+            e['縣市'] = '臺' + e['縣市'][1:]
+        if 'F' in e['住址']:
+            e['住址'] = e['住址'].replace('F', '樓')
+        if e['縣市'] == '臺中市' and '中港路' in e['住址']:
+            e['住址'] = e['住址'].replace('中港路', '臺灣大道')
+
+        print('更新資料:', e['姓名'], e['縣市'], e['住址'])
+
+with open('新地址資料.csv', 'w', newline='') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames=data[0].keys())
+    writer.writeheader()
+    for e in data:
+        writer.writerow(e)
